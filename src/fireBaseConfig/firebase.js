a92b634d990a6523e87783fb9e87cb3e5bb81316
DiegoHelal1 <D.Helal.A.A0@gmail.com>
2024-05-01 17:59:02 -0400

    Interfaz usuario

--- src/fireBaseConfig/firebase.js
+++ src/fireBaseConfig/firebase.js
@@ -3,16 +3,24 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Your web app's Firebase configuration -- Seba
+/*const firebaseConfig = {
   apiKey: "AIzaSyAXrpZ2-4ELCcroMkvr5bns1yak2bVVqQI",
   authDomain: "calidadinacap-cb101.firebaseapp.com",
   projectId: "calidadinacap-cb101",
   storageBucket: "calidadinacap-cb101.appspot.com",
   messagingSenderId: "366062973229",
   appId: "1:366062973229:web:e54309df4f514a42d563a4"
+};*/
+//Credenciales Diego
+const firebaseConfig = {
+  apiKey: "AIzaSyCjN-i8dJX0I8Tt2ijgAWo6-0zxN-Qg3mo",
+  authDomain: "backend-aseguramiento-calidad.firebaseapp.com",
+  projectId: "backend-aseguramiento-calidad",
+  storageBucket: "backend-aseguramiento-calidad.appspot.com",
+  messagingSenderId: "410197611386",
+  appId: "1:410197611386:web:0eb4936f3de909ae603cc3"
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
